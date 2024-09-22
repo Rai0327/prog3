9bba818aa3f8204a945bc180da9f3ae745288ac0
Rahul <rahul.ajay.iyer@gmail.com>
2024-09-21 21:02:38 -0500

    Started executeCritter method implementation

--- src/main/java/assignment/Interpreter.java
+++ src/main/java/assignment/Interpreter.java
@@ -11,13 +11,72 @@ import java.lang.*;
  * included API/ folder and the project description.
  */
 public class Interpreter implements CritterInterpreter {
+
+	private boolean bool;
+
 	public static void main(String args[]){
 		loadSpecies("FlyTrap.cri");
 	}
 	public static void executeCritter(Critter c) {
 		// obviously, your code should do something
-
+		String code = c.getCode();
+		int startIdx = code.getNextCodeLine();
+		bool = false;
+		for (int i = startIdx; i < code.size(); i++) {
+			String line = code.get(i);
+			String[] arr = line.split(" ");
+			String str = arr[0];
+			i = executeCode(c, str, i);
+			if (bool) {
+				c.setNextCodeLine(i + 1);
+				return;
+			}
+		}
 		return;
+	}
+
+	private int executeCode(Critter c, String str, int i) {
+		int idx = i;
+		switch(str) {
+			case "hop":
+				c.hop();
+				bool = true;
+				break;
+			case "left":
+				c.left();
+				bool = true;
+				break;
+			case "right":
+				c.right();
+				bool = true;
+				break;
+			case "infect":
+				if (arr.length > 1) {
+					c.infect(Integer.parseInt(arr[-1]));
+				} else {
+					c.infect();
+				}
+				bool = true;
+				break;
+			case "eat":
+				c.eat();
+				bool = true;
+				break;
+			case "go":
+				if (arr[-1].substring(0, 1).equals("+")) {
+					idx += Integer.parseInt(arr[-1].substring(1));
+					idx--;
+				} else if (arr[-1].substring(0, 1).equals("-")) {
+					idx -= Integer.parseInt(arr[-1].substring(1));
+					idx--;
+				} else if (arr[-1].substring(0, 1).equals("r")) {
+					executeCode(c, c.getReg(arr[-1], idx));
+				} else {
+					idx = Integer.parseInt(arr[-1]);
+				}
+				break;
+		}
+		return idx;
 	}
 
 	public static CritterSpecies loadSpecies(String filename) throws IOException {
